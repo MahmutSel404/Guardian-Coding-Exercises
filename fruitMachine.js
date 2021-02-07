fbccdab8a59124784b25de0e5e6c1153ad306c72
mahmutsel <mahmutsel29@gmail.com>
2021-02-07 12:58:20 +0000

    add slots' elements unique

--- fruitMachine.js
+++ fruitMachine.js
@@ -8,16 +8,16 @@ let no_of_free_play = 0;
 const symbols = ['black', 'white', 'green', 'yellow'];
 const no_of_slots = 4;
 
-// function check_difference(slots) {
-//   for (let i = 0; i < 4; i++) {
-//     for (let j = i + 1; j < 4; j++) {
-//       if (slots[i] === slots[j]) {
-//         return false;
-//       }
-//     }
-//   }
-//   return true;
-// }
+function check_difference(slots) {
+  for (let i = 0; i < 4; i++) {
+    for (let j = i + 1; j < 4; j++) {
+      if (slots[i] === slots[j]) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
 // function adjacentElements(slots) {
 //   // all slots elements same/
 //   if (slots[0] === slots[1] && slots[1] === slots[2] && slots[2] === slots[3]) {
@@ -39,37 +39,38 @@ function play(user_credit, money_in_machine) {
     let index = Math.floor(Math.random() * symbols.length);
     slots.push(symbols[index]);
   }
+  slots = ['white', 'green', 'black', 'yellow'];
   console.log(slots);
   //all slots elements same
-  if (slots[0] === slots[1]&& slots[1] === slots[2]&&slots[2] === slots[3]) {
+  if (slots[0] === slots[1] && slots[1] === slots[2] && slots[2] === slots[3]) {
     user_credit = user_credit + money_in_machine;
     money_in_machine = 0;
 
     return [user_credit, money_in_machine, no_of_free_play];
-    // }
-    // //all slots' elements different
-    // if (check_difference(slots)) {
-    //   user_credit = user_credit + money_in_machine / 2;
-    //   money_in_machine = money_in_machine / 2;
-    //   return user_credit, money_in_machine, no_of_free_play;
-    // }
-
-    // //// 2 or more adjacent slots are equal
-    // if (adjacentElements(slots)) {
-    //   if (money_in_machine >= 5 * play_credit) {
-    //     user_credit = user_credit + 5 * play_credit;
-    //     money_in_machine = money_in_machine - 5 * play_credit;
-    //   } else {
-    //     no_of_free_play =
-    //       no_of_free_play + (5 * play_credit - money_in_machine) / play_credit;
-    //     user_credit = user_credit + money_in_machine;
-    //     money_in_machine = 0;
-    //     console.log('>>>', no_of_free_play);
-
-    //   }
-    //   return user_credit, money_in_machine, no_of_free_play;
-    // }
   }
+  //all slots' elements different
+  if (check_difference(slots)) {
+    user_credit = user_credit + money_in_machine / 2;
+    money_in_machine = money_in_machine / 2;
+    return [user_credit, money_in_machine, no_of_free_play];
+  }
+
+  // //// 2 or more adjacent slots are equal
+  // if (adjacentElements(slots)) {
+  //   if (money_in_machine >= 5 * play_credit) {
+  //     user_credit = user_credit + 5 * play_credit;
+  //     money_in_machine = money_in_machine - 5 * play_credit;
+  //   } else {
+  //     no_of_free_play =
+  //       no_of_free_play + (5 * play_credit - money_in_machine) / play_credit;
+  //     user_credit = user_credit + money_in_machine;
+  //     money_in_machine = 0;
+  //     console.log('>>>', no_of_free_play);
+
+  //   }
+  //   return user_credit, money_in_machine, no_of_free_play;
+  // }
+
   return [user_credit, money_in_machine, no_of_free_play];
 }
 
