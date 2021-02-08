5dbf80a5e59380de5d915e6ac88e0271d246e237
mahmutsel <mahmutsel29@gmail.com>
2021-02-08 18:13:25 +0000

    complete functionality

--- game-of-life/sketch.js
+++ game-of-life/sketch.js
@@ -33,11 +33,48 @@ function draw() {
       let x = i * resolution;
       let y = j * resolution;
       if (grid[i][j] == 1) {
-
         fill(255);
-        stroke(0)
-        rect(x, y, resolution-1, resolution-1);
+        stroke(0);
+        rect(x, y, resolution - 1, resolution - 1);
       }
     }
   }
+  let next = make2Darray(cols, rows);
+
+  //Compute next based on grid
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      let state = grid[i][j];
+      // //Edges
+      // if (i == 0 || i == cols - 1 || j == 0 || j == rows - 1) {
+      //   next[i][j] = grid[i][j];
+      // }
+
+      //Count live neighbours
+
+      let neighbors = countNeighbors(grid, i, j);
+
+      if (state == 0 && neighbors == 3) {
+        next[i][j] = 1;
+      } else if (state == 1 && (neighbors < 2 || neighbors > 3)) {
+        next[i][j] = 0;
+      } else {
+        next[i][j] = state;
+      }
+    }
+  }
+  grid = next;
+}
+
+function countNeighbors(grid, x, y) {
+  let sum = 0;
+  for (let i = x - 1; i <= x + 1; i++) {
+    for (let j = y - 1; j <= y + 1; j++) {
+      let col = (i + cols) % cols;
+      let row = (j + rows) % rows;
+      sum += grid[col][row];
+    }
+  }
+  sum -= grid[x][y];
+  return sum;
 }
